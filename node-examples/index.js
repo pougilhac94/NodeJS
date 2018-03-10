440677363afba6ee625f55fa44072321911428f2
pougilhac <christian.collenot@gmail.com>
2018-03-10 15:38:30 +0100

    Node Callbacks and Error Handling

--- node-examples/index.js
+++ node-examples/index.js
@@ -1,17 +1,21 @@
 var rect = require('./rectangle');
 
 function solveRect(l,b) {
-    console.log("Calculer le rectangle avec l = " + l + " and b = " + b);
-
-    if (l <= 0 || b <= 0) {
-        console.log("Les dimensions du rectangle doivent être supérieures à zéro:  l = "
-               + l + ",  and b = " + b);
-    }
-    else {
-	    console.log("La surface du rectangle est de " + rect.area(l,b));
-	    console.log("Le périmètre du rectangle est de " + rect.perimeter(l,b));
-    }
-}
+    console.log("Calculer le rectangle avec l = "
+                + l + " et b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERREUR: ", err.message);
+	    }
+        else {
+            console.log("La surface du rectangle de  dimensions l = "
+                + l + " et b = " + b + " est " + rectangle.area());
+            console.log("Le périmètre du rectangle de dimensions l = "
+                + l + " et b = " + b + " est " + rectangle.perimeter());
+        }
+    });
+    console.log("Cette instruction d'affichage est exécutée immédiatement après l'appel de rect()");
+};
 
 solveRect(2,4);
 solveRect(3,5);
--- node-examples/rectangle.js
+++ node-examples/rectangle.js
@@ -1,3 +1,15 @@
-exports.perimeter =  (x, y) => (2*(x+y));
-
-exports.area = (x, y) => (x*y);
+module.exports = (x,y,callback) => {
+    if (x <= 0 || y <= 0)
+        setTimeout(() => 
+            callback(new Error("Les dimensions du rectangle doivent être supérieures à zéro:  l = "
+                + x + ", et b = " + y), 
+            null),
+            2000);
+    else
+        setTimeout(() => 
+            callback(null, {
+                perimeter: () => (2*(x+y)),
+                area:() => (x*y)
+            }), 
+            2000);
+  }
