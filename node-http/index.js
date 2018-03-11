c22d5a43648ed17f1d2ad8f51208adef5b315be7
pougilhac <christian.collenot@gmail.com>
2018-03-11 09:52:13 +0100

    Node HTTP Example 2

--- node-http/index.js
+++ node-http/index.js
@@ -1,14 +1,49 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
+
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Bonjour le monde !!</h1></body></html>');
-})
+    console.log('Requête pour ' + req.url + ' par la méthode ' + req.method);
+  
+    if (req.method == 'GET') {
+      var fileUrl;
+      if (req.url == '/') fileUrl = '/index.html';
+      else fileUrl = req.url;
+  
+      var filePath = path.resolve('./public'+fileUrl);
+      const fileExt = path.extname(filePath);
+      if (fileExt == '.html') {
+        fs.exists(filePath, (exists) => {
+          if (!exists) {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>Error 404: ' + fileUrl + 
+                        ' absent</h1></body></html>');
+            return;
+          }
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html');
+          fs.createReadStream(filePath).pipe(res);
+        });
+      }
+      else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1>Error 404: ' + fileUrl + 
+               " n'est pas un fichier HTML</h1></body></html>");
+      }
+    }
+    else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1>Error 404: ' + req.method + 
+                " n'est pas supporté</h1></body></html>");
+    }
+  })
 
 server.listen(port, hostname, () => {
   console.log(`Le serveur tourne à l'adresse http://${hostname}:${port}/`);
