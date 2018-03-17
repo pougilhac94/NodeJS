f0e1206304056ddb6900c426a39c8ed5b562867e
pougilhac <christian.collenot@gmail.com>
2018-03-17 12:42:42 +0100

    Assignment  1

--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -19,11 +19,34 @@ dishRouter.route('/')
 })
 .put((req, res, next) => {
     res.statusCode = 403;
-    res.end('PUT non supporté sur /dishes');
+    res.end("PUT n'est pas supporté sur /dishes");
 })
 .delete((req, res, next) => {
     res.end('Suppression de tous les dishes');
 });
+
+dishRouter.route('/:dishId/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Vous enverra le détail de '+ req.params.dishId );
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST non supporté sur /dishes/' + req.params.dishId );
+})
+.put((req, res, next) => {
+    res.write('Mise à jour du  dish: ' + req.params.dishId + '\n');
+    res.end('Va mettre à jour le dish ' + req.body.name + 
+        ' en détail : ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Suppression du dish ' + req.params.dishId);
+});
+
 
 module.exports = dishRouter;
 
