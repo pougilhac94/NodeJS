a5604c9b8dac13ef4dbfce45b87cd391f94aee6b
pougilhac <christian.collenot@gmail.com>
2018-03-24 15:33:18 +0100

    Mongoose Part 3
    Avec un ajour personnel dans les exemples de maj

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -12,30 +12,45 @@ connect.then((client) => {
     var db = mongoose.connection;
 
     Dishes.create({
-        name: 'Uthapizza',
-        description: 'Test'
+        name: 'Uthappizza',
+        description: 'test'
     })
     .then((dish) => {
         console.log(dish);
-        
+
         return Dishes.findByIdAndUpdate(dish._id, {
-                $set: {
-                    description: 'Updated Test'
-                }
-            }, {
-                new: true
-            })
-            .exec();
+            $set: { description: 'Updated test en recherchant le dish par id'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
     .then((dish) => {
         console.log(dish);
+        dish.description='Updated test directement sur le dish en mémoire';
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
         return db.collection('dishes').drop();
     })
     .then(() => {
-        return db.close();        
+        return db.close();
     })
     .catch((err) => {
-
+        console.log(err);
     });
 
 });
